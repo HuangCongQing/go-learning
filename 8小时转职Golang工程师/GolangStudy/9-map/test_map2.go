5cda8c5da96a8d32338e9c8f4e2f432a95ec9352
hcq <1756260160@qq.com>
2023-08-20 15:45:10 +0800

    feat: map使用方式

--- 8小时转职Golang工程师/GolangStudy/9-map/test_map2.go
+++ 8小时转职Golang工程师/GolangStudy/9-map/test_map2.go
@@ -1,10 +1,10 @@
 /*
- * @Description: map使用方式
+ * @Description: map使用方式 https://www.yuque.com/huangzhongqing/lang/qso8oc#V78zN
  * @Author: HCQ
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-02-14 23:08:07
- * @LastEditTime: 2021-02-16 12:03:47
+ * @LastEditTime: 2023-08-20 15:44:19
  * @FilePath: /go-learning/8小时转职Golang工程师/GolangStudy/9-map/test_map2.go
  */
 
@@ -15,7 +15,7 @@ import "fmt"
 
 // 遍历
 func printMap(cityMap map[string]string) {
-	//cityMap 是一个引用传递
+	//cityMap 是一个引用传递<<<<<<<<<<,
 	for key, value := range cityMap {
 		fmt.Println("key = ", key)
 		fmt.Println("value = ", value)
@@ -29,18 +29,18 @@ func ChangeValue(cityMap map[string]string) {
 func main() {
 	cityMap := make(map[string]string)
 
-	//添加
+	//1 添加
 	cityMap["China"] = "Beijing"
 	cityMap["Japan"] = "Tokyo"
 	cityMap["USA"] = "NewYork"
 
-	//遍历
+	//2 遍历
 	printMap(cityMap)
 
-	//删除
+	//3 删除
 	delete(cityMap, "China")
 
-	//修改
+	//4 修改
 	cityMap["USA"] = "DC"
 	ChangeValue(cityMap)
 

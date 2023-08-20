a23b0174f2737eadb97f861d67e8994c791064e3
hcq <1756260160@qq.com>
2023-08-20 14:55:56 +0800

    feat: defer语句调用顺序  先进栈再出栈

--- 8小时转职Golang工程师/GolangStudy/7-defer/defer.go
+++ 8小时转职Golang工程师/GolangStudy/7-defer/defer.go
@@ -1,10 +1,10 @@
 /*
- * @Description: defer关键字
+ * @Description: defer关键字 https://www.yuque.com/huangzhongqing/lang/qso8oc#02466cc4
  * @Author: HCQ
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-02-14 23:08:07
- * @LastEditTime: 2021-02-16 11:30:42
+ * @LastEditTime: 2023-08-20 14:55:46
  * @FilePath: /go-learning/8小时转职Golang工程师/GolangStudy/7-defer/defer.go
  */
 package main

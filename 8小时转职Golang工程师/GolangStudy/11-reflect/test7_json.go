9e541bb7d33b8481d9b3ee2dd7408453a19b8eea
hcq <1756260160@qq.com>
2023-08-20 20:35:38 +0800

    feat: 结构体转换成json

--- 8小时转职Golang工程师/GolangStudy/11-reflect/test7_json.go
+++ 8小时转职Golang工程师/GolangStudy/11-reflect/test7_json.go
@@ -1,21 +1,21 @@
 /*
- * @Description: json
+ * @Description: 结构体转换成json https://www.yuque.com/huangzhongqing/lang/qso8oc#Ume2q
  * @Author: HCQ
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-02-14 23:08:07
- * @LastEditTime: 2021-02-16 12:55:51
+ * @LastEditTime: 2023-08-20 20:35:31
  * @FilePath: /go-learning/8小时转职Golang工程师/GolangStudy/11-reflect/test7_json.go
  */
 package main
 
 import (
-	"encoding/json"
+	"encoding/json"  // json格式 jsonStr, err := json.Marshal(movie)
 	"fmt"
 )
 
 type Movie struct {
-	Title  string   `json:"title"`
+	Title  string   `json:"title"` // 匹配标签里的key是json
 	Year   int      `json:"year"`
 	Price  int      `json:"rmb"`
 	Actors []string `json:"actors"`
@@ -25,7 +25,7 @@ func main() {
 	movie := Movie{"喜剧之王", 2000, 10, []string{"xingye", "zhangbozhi"}}
 
 	//编码的过程  结构体---> json
-	jsonStr, err := json.Marshal(movie)
+	jsonStr, err := json.Marshal(movie) // 匹配标签里的key是json
 	if err != nil {
 		fmt.Println("json marshal error", err)
 		return

d43f1d024cac292e6b2d57098319d4ebebdcfe22
hcq <1756260160@qq.com>
2023-08-24 02:08:29 +0800

    demo

---
+++ modules-go/gorm/example_sqlite.go
@@ -0,0 +1,50 @@
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2023-08-24 02:04:59
+ * @LastEditTime: 2023-08-24 02:08:16
+ * @FilePath: /go-learning/modules-go/gorm/example_sqlite.go
+ */
+package main
+
+import (
+  "fmt"
+  "gorm.io/gorm"
+  "gorm.io/driver/sqlite"
+)
+
+type Product struct {
+  gorm.Model
+  Code  string
+  Price uint
+}
+
+func main() {
+  fmt.Println(" 打开数据库sqlite!")
+  db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{}) // 打开数据库
+  if err != nil {
+    panic("failed to connect database")
+  }
+
+  // 迁移 schema
+  db.AutoMigrate(&Product{})
+
+  // Create
+  db.Create(&Product{Code: "D42", Price: 100})
+
+  // Read
+  var product Product
+  db.First(&product, 1) // 根据整型主键查找
+  db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
+
+  // Update - 将 product 的 price 更新为 200
+  db.Model(&product).Update("Price", 200)
+  // Update - 更新多个字段
+  db.Model(&product).Updates(Product{Price: 200, Code: "F42"}) // 仅更新非零值字段
+  db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
+
+  // Delete - 删除 product
+  db.Delete(&product, 1)
+}
